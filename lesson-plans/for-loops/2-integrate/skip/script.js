fc793b0e7aae1f876eb0cce3cf8247b66a5a335b
AdilCodeBX <jihadmdna@gmail.com>
2024-03-07 15:26:16 +0100

    changes for-loops

--- lesson-plans/for-loops/2-integrate/skip/script.js
+++ lesson-plans/for-loops/2-integrate/skip/script.js
@@ -1,11 +1,25 @@
 import { readNumber, readString, display } from '../../../../lib/dom-io.js';
 
-document.getElementById(__).addEventListener('click', () => {
+document.getElementById('skip-them').addEventListener('click', () =>
+ {
   debugger;
 
   // read user values
+  let numberSkip = readNumber('skip-size');
+  let text = readString('user-text');
+  console.log(text);
 
+  let skipText = '';
+
+if (numberSkip <= 0) {
+     console.error('the number of character to skip must be more than zero.');
+} else {
   // create a new string with skipped characters
-
+  for (let i = 0; i < text.length; i += numberSkip) {
+      skipText += text[i];
+       console.log(skipText);
+  }
+}
   // display the skipped string
+  display('skipped-output', skipText)
 });
